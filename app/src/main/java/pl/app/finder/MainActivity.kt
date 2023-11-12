24d8c8fe549734c4ecd02b1f9286d55683a256a9
Rafał Grzegorzek <grzegorzek2002@gmail.com>
2023-11-12 14:53:26 +0100

    Działające skanowanie kodu QR

--- app/src/main/java/pl/app/finder/MainActivity.kt
+++ app/src/main/java/pl/app/finder/MainActivity.kt
@@ -1,34 +1,29 @@
 package pl.app.finder
 
-import android.net.Uri
-import android.widget.Toast
 import android.content.Intent
-import android.provider.MediaStore
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
-import android.widget.ImageView
-import android.app.AlertDialog
-import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.integration.android.IntentIntegrator
 
 class MainActivity : AppCompatActivity() {
     private lateinit var btnCam: Button
-    private lateinit var imageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btnCam = findViewById(R.id.btnCam)
-        imageView = findViewById(R.id.imageView)
 
         btnCam.setOnClickListener {
-            try {
-                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                startActivity(intent)
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
+            // Inicjalizacja skanera ZXing
+            val integrator = IntentIntegrator(this)
+            integrator.setPrompt("Zeskanuj kod QR") // Komunikat zachęty dla użytkownika
+            integrator.setBeepEnabled(true) // Włączenie dźwięku przy skanowaniu
+            integrator.setOrientationLocked(false) // Odblokowanie obrotu ekranu podczas skanowania
+            integrator.initiateScan() // Uruchomienie aktywności skanowania
         }
 
         val buttonInfo = findViewById<Button>(R.id.button4)
@@ -54,11 +49,42 @@ class MainActivity : AppCompatActivity() {
                 startActivity(mapIntent)
             } else {
                 // Jeśli aplikacja Google Maps nie jest zainstalowana, otwórz stronę internetową
-                val webIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/maps/search/Żabka+nowy+sącz/@49.6026939,20.708025,13z/data=!3m1!4b1?entry=ttu"))
+                val webIntent = Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://www.google.com/maps/search/Żabka+nowy+sącz/@49.6026939,20.708025,13z/data=!3m1!4b1?entry=ttu")
+                )
                 startActivity(webIntent)
             }
         }
+    }
 
+    // Funkcja obsługująca wynik skanowania kodu QR
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Przekazanie danych do skanera ZXing
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+
+        // Sprawdzenie, czy wynik skanowania jest poprawny
+        if (result != null) {
+            if (result != null) {
+                if (result.contents != null) {
+                    val scannedText = result.contents
+
+                    val alertDialogBuilder = AlertDialog.Builder(this)
+                    alertDialogBuilder.setTitle("Odczytano kod QR")
+                    alertDialogBuilder.setMessage("Treść kodu: $scannedText")
+                    alertDialogBuilder.setPositiveButton("OK") { dialog, which -> dialog.dismiss() }
+
+                    val alertDialog = alertDialogBuilder.create()
+                    alertDialog.show()
+
+                    // Tutaj możesz podjąć dalsze działania w zależności od odczytanego tekstu
+                }
+            }
+
+        }
     }
 }
+
 
